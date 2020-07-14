74db685158a73d621466356c80bba5185314f4f2
Roman Mavrin <roma-mrv@yandex.ru>
2020-07-15 02:15:15 +0300

    first commit

--- src/js/Card.js
+++ src/js/Card.js
@@ -1,11 +1,4 @@
-class Card {
-  /*
-     Можно лучше: лучше передавать не отдельные параметры, а сразу весь объект с данными карточки,
-     т.к. представьте что у карточки появится ещё одно свойство (например author) которое нужно будет отобразить
-     Если у нас создание карточки вызывается как createCard(name,link), придется во всех местах
-     где вызывается создание карточки переписывать её вызов с createCard(name,link)  на createCard(name, link, author)
-     Если ли же мы передаем просто объект карточки в функцию ( createCard(cardData) ) нам придется гораздо меньше менять программу
-   */
+export class Card {
   constructor(name, link, likes, openImageCallback) {
     this.name = name;
     this.link = link;
@@ -13,9 +6,6 @@ class Card {
     this.openImageCallback = openImageCallback;
     this.openCardImage = this.openCardImage.bind(this);
     this.removeCard = this.removeCard.bind(this);
-
-
-
   }
 
   createCard() {
@@ -35,16 +25,18 @@ class Card {
     element.insertAdjacentHTML("afterbegin", marking.trim());
     const newTask = element.firstElementChild;
     newTask.querySelector(".place-card__name").textContent = this.name;
-    newTask.querySelector(".place-card__image").style.backgroundImage = "url(" + this.link + ")";
-    newTask.querySelector(".place-card__image").setAttribute("data-url", this.link);
+    newTask.querySelector(".place-card__image").style.backgroundImage =
+      "url(" + this.link + ")";
+    newTask
+      .querySelector(".place-card__image")
+      .setAttribute("data-url", this.link);
     this.likeElem = newTask.querySelector(".place-card__like-icon");
     this.deleteElem = newTask.querySelector(".place-card__delete-icon");
     this.openImageElem = newTask.querySelector(".place-card__image");
     this.newTask = newTask;
-    this.addListeners()
-    return newTask
+    this.addListeners();
+    return newTask;
   }
-
 
   likeCard() {
     this.classList.toggle("place-card__like-icon_liked");
@@ -57,20 +49,19 @@ class Card {
 
   openCardImage() {
     if (event.target.classList.contains("place-card__image")) {
-      this.openImageCallback(this.link)
+      this.openImageCallback(this.link);
     }
   }
 
   addListeners() {
-    this.likeElem.addEventListener('click', this.likeCard);
+    this.likeElem.addEventListener("click", this.likeCard);
     this.deleteElem.addEventListener("click", this.removeCard);
     this.openImageElem.addEventListener("click", this.openCardImage);
   }
 
   removeListeners() {
     this.deleteElem.removeEventListener("click", this.removeCard);
-    this.likeElem.removeEventListener('click', this.likeCard);
+    this.likeElem.removeEventListener("click", this.likeCard);
     this.openImageElem.removeEventListener("click", this.openCardImage);
   }
-
 }
--- src/js/CardList.js
+++ src/js/CardList.js
@@ -1,4 +1,4 @@
-class CardList {
+export class CardList {
   constructor(container, createCardCallback) {
     this.container = container;
     this.createCardCallback = createCardCallback;
